a3184fb31fd213ecd571b0ddc116a5b77cc2d3f4
Jeferson <jeferson_014_17@hotmail.com>
2022-08-28 23:44:34 -0500

    Se agrego el navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <NavBar/>
+      <div className='container' style={{marginTop:'60px'}}>
       <h1>Idea Verastegui Quispe</h1>
+      </div>
+     
     </div>
   );
 }
