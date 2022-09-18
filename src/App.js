dbe66bb24aef847b6672952ad33c5298f5cd9710
Jeferson <jeferson_014_17@hotmail.com>
2022-09-17 21:31:10 -0500

    Se agrega evento de itemcount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from './Components/NavBar';
 import ItemListContainer from './Components/ItemListContainer';
 import ItemDetailContainer from './Components/ItemDetailContainer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cart from './Components/Cart';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/' element={<ItemListContainer/>} />
         <Route exact path='/category/:id' element={<ItemListContainer/>} />
         <Route exact path='/item/:id' element={<ItemDetailContainer/>} />
+        <Route exact path='/cart' element={<Cart/>} />
       </Routes>
      </BrowserRouter>
 
--- src/Components/ItemDetail.jsx
+++ src/Components/ItemDetail.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import ItemCount from "./ItemCount";
 
 const ItemDetail = ({ item }) => {
+
+    const [items, setItemCart] = useState([]);
+
+    const onAdd=(quantityToAdd)=>{
+        items.push({...item,quantity: quantityToAdd});
+        setItemCart(items);
+        console.log(items);
+    }
+
     return (
         <div className="row">
             <div className="col-md-6">
@@ -15,7 +25,13 @@ const ItemDetail = ({ item }) => {
                 </p>
                 <h4 className="pb-4 pt-2"><mark>S/. {item.precio}</mark></h4>
 
-                <ItemCount stock={item.stock} initial={1} onAdd={(count) => { console.log(count); }}></ItemCount>
+                <ItemCount stock={item.stock} initial={1} onAdd={onAdd}></ItemCount>
+                <br/>
+                <br/>
+                <br/>
+
+                <Link to={'/cart'} className="btn btn-primary col-5">Comprar Ahora</Link>
+
             </div>
         </div>
     );
